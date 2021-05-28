e9854775c628356c2d787c4f07b1f0e5b790e843
trash <73361653+galenddavis@users.noreply.github.com>
2021-05-28 15:32:46 -0400

    Finished part 1 of tutorial

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable require-jsdoc */
 import React from 'react';
+import '../styles/global.css';
 
 
 // export default function App({Component, pageProps}) {
